37ca6aff801ef035792a1f194448576861d7ddd2
sohailtanveer01 <sohailtanveer769@gmail.com>
2022-09-25 02:17:01 +0530

    changes done by sohail tanveer

--- index.js
+++ index.js
@@ -4,13 +4,28 @@ require('dotenv').config()
 
 var app = express();
 
+var multer = require('multer')
+const upload = multer({dest: "public/files" })
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
-
+app.use(express.json())
+app.use(express.urlencoded({
+  extended:true
+}))
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+
+app.post('/api/fileanalyse', upload.single('upfile'), function(req,res){
+  console.log(req.file)
+  const {originalname,mimetype,size} = req.file
+  res.json({
+    name : originalname,
+    type : mimetype,
+    size : size
+  })
+})
 
 
 
